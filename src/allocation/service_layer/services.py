ab49c11280b3bd4d0176e0337ba925404d6a0212
Otávio Leite <otavio.leite.ufu@gmail.com>
2021-07-08 16:01:12 -0300

    End of chapter 5

--- src/allocation/service_layer/services.py
+++ src/allocation/service_layer/services.py
@@ -1,15 +1,30 @@
+from datetime import date
+from typing import Optional
 from allocation.adapters.repository import AbstractRepository
-from allocation.domain.model import OrderLine
 from allocation.domain import model
 
 
 class InvalidSku(Exception):
     pass
 
+
 def is_valid_sku(sku, batches):
     return sku in {b.sku for b in batches}
 
-def allocate(line: OrderLine, repo: AbstractRepository, session) -> str:
+
+def add_batch(
+    ref: str, sku: str, qty: int, eta: Optional[date],
+    repo: AbstractRepository, session
+) -> None:
+    repo.add(model.Batch(ref, sku, qty, eta))
+    session.commit()
+
+
+def allocate(
+    orderid: str, sku: str, qty: int,
+    repo: AbstractRepository, session
+) -> str:
+    line = model.OrderLine(orderid, sku, qty)
     batches = repo.list()
     if not is_valid_sku(line.sku, batches):
         raise InvalidSku(f"Invalid sku {line.sku}")
